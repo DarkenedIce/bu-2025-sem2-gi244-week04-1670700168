a416503b1b68f278a8b244efb7de23d2f84f29ef
DarkendIce <pechto.aroo@bumail.net>
2026-02-06 16:41:57 +0700

    week04

--- Assets/Scripts/DestroyOutOfBound.cs
+++ Assets/Scripts/DestroyOutOfBound.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class DestroyOutOfBound : MonoBehaviour
 {
+    public float bound = 20f;
     // Update is called once per frame
     void Update()
     {
-
+        if (transform.position.z < -bound || transform.position.z > bound)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,55 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float walkSpeed = 10.0f;
+    public float walkRange = 10.0f;
+
+    public GameObject foodPrefab;
+
+    private InputAction moveAction;
+    private InputAction shootAction;
+
     private void Awake()
     {
+        moveAction = InputSystem.actions.FindAction("Move");
+        shootAction = InputSystem.actions.FindAction("Shoot");
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        var horizontalInput = moveAction.ReadValue<Vector2>().x;
+        transform.Translate(horizontalInput * walkSpeed * Time.deltaTime * Vector3.right);
 
+        if(transform.position.x < -walkRange)
+        {
+            transform.position = new Vector3(-walkRange, transform.position.y, transform.position.z);
+        }
+        else if(transform.position.x > walkRange)
+        {
+            transform.position = new Vector3(walkRange, transform.position.y, transform.position.z);
+        }
+
+        if(shootAction.triggered)
+        {
+            Instantiate(foodPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.DrawWireSphere(transform.position, 1f);
+        //Gizmos.color = Color.blue;
+        //Gizmos.DrawLine(transform.position, Camera.main.transform.position);
+
+        Gizmos.color = Color.red;
+        Vector3 left = new Vector3(-walkRange, transform.position.y, transform.position.z);
+        Vector3 right = new Vector3(walkRange, transform.position.y, transform.position.z);
+        Gizmos.DrawLine(left, right);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(left, 0.5f);
+        Gizmos.DrawWireSphere(right, 0.5f);
     }
 }
